12e51c94dbe8d0e3793a4120df8bd57fd6de337e
Apollo4634 <wenyuexin@163.com>
2018-11-23 21:41:14 +0800

    11232141

---
+++ src/linked_list/medium/AddTwoNumbers.java
@@ -0,0 +1,31 @@
+package linked_list.medium;
+
+/**
+ * @author Apollo4634
+ * @creation 2018/11/23
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+
+
+public class AddTwoNumbers {
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		
+		
+		
+		return new ListNode(0);
+	}
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) { val = x; }
+}
