a90f552da0d010311a27e78926552ca775f8e997
Apollo4634 <wenyuexin@163.com>
2018-11-26 23:56:11 +0800

    11262356

---
+++ src/hash_table/medium/LongestSubstring.java
@@ -0,0 +1,34 @@
+package hash_table.medium;
+
+/** 
+ * @author Apollo4634 
+ * @creation 2018/11/26
+ */
+
+/**
+ * Tilte: 
+ * - Longest Substring Without Repeating Characters
+ * 
+ * Difficulty:
+ * - Medium
+ * 
+ * 
+ */
+public class LongestSubstring {
+	public int lengthOfLongestSubstring(String s) {
+		int maxLen = 0;
+		int j = 0;
+		
+		for(int i=0; i<s.length(); i++) {
+			
+		}
+		
+		
+		return 0;
+    }
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
