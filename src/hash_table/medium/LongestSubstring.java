6581243afc0cc5ed8684e17cfcd7d3225107de01
Apollo4634 <apollo4634@gmail.com>
2018-11-27 19:16:37 +0800

    11271916

--- src/hash_table/medium/LongestSubstring.java
+++ src/hash_table/medium/LongestSubstring.java
@@ -1,9 +1,13 @@
 package hash_table.medium;
 
+import java.util.HashMap;
+
 /** 
  * @author Apollo4634 
  * @creation 2018/11/26
- * 
+ */
+
+/**
  * Tilte: 
  * - Longest Substring Without Repeating Characters
  * 
@@ -14,29 +18,41 @@ package hash_table.medium;
 
 
 public class LongestSubstring {
+	
+	static boolean inTheSet(HashSet<Character> set, char c) {
+		boolean flag = false;
+		Character character = Character.valueOf(c);
+		flag = set.contains(character);
+		if(!flag) {
+			set.add(character);
+		}
+		return flag;
+	}
+	
 	public static int lengthOfLongestSubstring(String s) {
 		int maxLen = 0;
 		int strLen = -1;
 		//int j = 0;
 		
 		char c = ' ';
-		char char_s = ' ';
-		boolean flag = true;
+		HashMap<Character,Integer> map = new HashMap<>();
 		for(int i=0; i<s.length(); i++) {
-			if (flag) {
-				char_s = s.charAt(i);
-				if (c != char_s) {
-					c = char_s;
-					strLen++;
-				} else {
-					//flag = false;
-					if(maxLen<strLen+1) 
-						maxLen = strLen+1;
-					strLen = 0;
-				}
-			} else {
-				
+			c = s.charAt(i);
+			Character character = Character.valueOf(c);
+			Integer integer = Integer.valueOf(i);
+			
+			//这里需要判断新来的char是否在keys中
+			if (!set.contains(character)) { //如果不在
+				map.put(character,integer);
+				strLen++;
+			} else { //如果在
+				if(maxLen<map.size()) 
+					maxLen = strLen;
+				//更新map
+				map = new HashMap<Character,Integer>();
+				strLen = 0;
 			}
+
 		}
 		
 		
@@ -45,7 +61,7 @@ public class LongestSubstring {
 	
 	
 	public static void main(String[] args) {
-		String str = "abcabcbb";
+		String str = "bbbbb";
 		int len = LongestSubstring.lengthOfLongestSubstring(str);
 		System.out.println(len);
 	}
