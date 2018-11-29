bad34680d8730fdc4e472d33d0d4bcd596bdd07d
Apollo4634 <apollo4634@gmail.com>
2018-11-29 23:32:42 +0800

    11292332

--- src/hash_table/medium/LongestSubstring2.java
+++ src/hash_table/medium/LongestSubstring2.java
@@ -8,47 +8,44 @@ import java.util.HashMap;
  *
  * Tilte: Longest Substring Without Repeating Characters
  * 
- * Runtime: 33 ms, faster than 62.50% of Java online submissions
+ * Runtime: 24 ms, faster than 91.82% of Java online submissions
+ * Runtime: 42 ms, faster than 43.79% of Java online submissions
+ * Runtime: 34 ms, faster than 59.28% of Java online submissions
  */
 
 public class LongestSubstring2 {
 	
 	public static int lengthOfLongestSubstring(String s) {
-		int maxLen = 0;
-		int strLen = 0;
+		Integer one = Integer.valueOf(1);
+		Integer maxLen = Integer.valueOf(0);
+		Integer strLen = Integer.valueOf(0);
 		Integer idx_left = Integer.valueOf(0);
-		Integer idx_tmp = null;
+		Integer idx_tmp = Integer.valueOf(0);
 		
 		HashMap<Character,Integer> map = new HashMap<>();
-		
 		for(int i=0; i<s.length(); i++) {
 			Character character = Character.valueOf(s.charAt(i));
 			Integer integer = Integer.valueOf(i);
 			
-			if (!map.containsKey(character)) { //判断char是否在keys中, 如果不在
-				map.put(character,integer);
-				strLen += 1;
-				if(maxLen<strLen) maxLen = strLen;
+			if (!map.containsKey(character)) { //判断char是否在keys中
+				strLen += one;
 			} else { //如果在
 				idx_tmp = map.get(character);
 				if(idx_tmp<idx_left) {
-					map.put(character,integer);//更新map
-					strLen += 1;
-					if(maxLen<strLen) maxLen = strLen;
+					strLen += one;
 				} else {
-					strLen = i - idx_tmp.intValue();
-					if(maxLen<strLen) maxLen = strLen;
-					idx_left = idx_tmp + 1;
-					 
-					map.put(character,integer);//更新map
+					strLen = integer - idx_tmp;
+					idx_left = idx_tmp + one;
 				}
 			}
+			map.put(character,integer);//更新map				
+			if(maxLen<strLen) maxLen = strLen;
 		}
 		return maxLen;
     }
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		//String str = "bbbbb";
 		//String str = "abcabcbb";
 		//String str = "pwwkew";
