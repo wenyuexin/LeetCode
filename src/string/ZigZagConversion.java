058d266f65b403cc179ea2b804e99c9b2a369d48
Apollo4634 <apollo4634@gmail.com>
2018-12-09 16:27:38 +0800

    12091627

---
+++ src/string/ZigZagConversion.java
@@ -0,0 +1,20 @@
+package string;
+
+/** 
+ * @author Apollo4634 
+ * @creation 2018/12/09 16:12
+ */
+
+public class ZigZagConversion {
+	
+	//Solution
+	public String convert(String s, int numRows) {
+
+		return "";
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
