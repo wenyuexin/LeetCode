b1d024327148ba1d1c1dd15651f4e55e39b02932
Apollo4634 <wenyuexin@163.com>
2018-12-12 00:43:59 +0800

    12120043

--- src/string/ZigZagConversion3.java
+++ src/string/ZigZagConversion3.java
@@ -20,29 +20,56 @@ import java.util.Arrays;
 public class ZigZagConversion3 {
 	
 	//Solution
-	public String convert(String s, int numRows) {	
-		int sLen = s.length();
-		if(sLen<2 || numRows==1) return s;
-		char[] sCharArr = s.toCharArray();
-		
-		//sLen==2
-		char[] strCharArr = new char[sLen];
-		if(sLen==2) {
-			for (int i=0; i<sLen; i++) {
-				strCharArr[i] = sCharArr[i];
-			}
-		}
-		
+	private int[] getStrLenArr(int sLen, int numRows) {
 		int groupLen = numRows-1;
 		int nGroup = sLen/groupLen;
 		//int nGroup2 = sLen/(2*groupLen);
 		int nRemain = sLen%groupLen;
 		
-		int[] strLenArr = new int[numRows]; 
-		strLenArr[0] = (int) Math.ceil(sLen/(2.0*groupLen));
-		strLenArr[numRows-1] = (int) Math.ceil(nGroup/2.0);
+		int[] strLenArr = new int[numRows];
+		if(nGroup%2==0) {
+			if(nRemain<2) {
+				Arrays.fill(strLenArr, 0, numRows, nGroup);
+			} else {
+				Arrays.fill(strLenArr, 0, nRemain, nGroup+1);
+				Arrays.fill(strLenArr, nRemain, numRows, nGroup);
+			}
+		} else {
+			if(nRemain<2) {
+				Arrays.fill(strLenArr, 0, numRows, nGroup);
+			} else {
+				Arrays.fill(strLenArr, 0, numRows-nRemain, nGroup);
+				Arrays.fill(strLenArr, numRows-nRemain, numRows, nGroup);
+			}
+		}
 		
-		Arrays.fill(strLenArr, 1, numRows-1, nGroup);
+		strLenArr[0] = (int) Math.ceil(sLen/(2.0*groupLen));
+		if(nGroup%2==1 && nRemain>0) {
+			strLenArr[numRows-1] = strLenArr[0] + 1;			
+		} else {
+			strLenArr[numRows-1] = strLenArr[0];
+		}
+
+		return strLenArr;
+	}
+	
+	public String convert(String s, int numRows) {	
+		int sLen = s.length();
+		if(sLen<2 || numRows==1) return s;
+		char[] sCharArr = s.toCharArray();
+		char[] strCharArr = new char[sLen];
+		
+		System.out.println("test"+sLen);
+		
+		/*
+		int groupLen = numRows-1;
+		int nGroup = sLen/groupLen;
+		//int nGroup2 = sLen/(2*groupLen);
+		int nRemain = sLen%groupLen;
+		*/
+		
+		int[] strLenArr = getStrLenArr(sLen, numRows); 
+		Arrays.toString(strLenArr);
 		
 		
 		String str = "";
@@ -54,11 +81,11 @@ public class ZigZagConversion3 {
 	public static void main(String[] args) {
 		//String s = "PAYPALISHIRING"; //4 "PINALSIGYAHRPI"
 		//String s = "PAYPALISHIRING"; //3 "PAHNAPLSIIGYIR"
-		String s = "a"; 
-		int numRows = 1;
+		String s = "123456789"; 
+		int numRows = 2;
 		
 		long t1 = System.nanoTime();
-		String str = new ZigZagConversion2().convert(s, numRows);
+		String str = new ZigZagConversion3().convert(s, numRows);
 		long t2 = System.nanoTime();
 		
 		System.out.println("intput: "+s);
@@ -66,5 +93,6 @@ public class ZigZagConversion3 {
 		System.out.println(str.equals("PINALSIGYAHRPI"));
 		System.out.println(str.equals("PAHNAPLSIIGYIR"));
 		System.out.println("Rumtime: "+(t2-t1)/1.0E6+" ms");
+		
 	}
 }
