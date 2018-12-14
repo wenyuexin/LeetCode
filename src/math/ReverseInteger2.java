47618d72d3ea3947dd704c7ec8aca1e7ba5bf059
Apollo4634 <wenyuexin@163.com>
2018-12-15 01:27:05 +0800

    12150127

--- src/math/ReverseInteger2.java
+++ src/math/ReverseInteger2.java
@@ -7,29 +7,57 @@ package math;
  * No.7 Reverse Integer
  * 
  * 解题思路：
+ * 转化为字符串进行处理，并使用StringBuilder类的字符串反转实现数字的反转
+ * 具体操作时还需要注意符号位的处理
  * 
- * 
+ * Solution2:
+ * 基本方法和solution1差不多，区别在于 
+ * 使用了自定义的函数替换StringBuilder类的字符串反转功能
  */
 
 public class ReverseInteger2 {
 	
-	//Solution
+	//Solution1
 	public int reverse(int x) {
 		if(x==Integer.MIN_VALUE) return 0;
 		int sgn = (x>0)?1:-1;
 		String numReverseStr = new StringBuilder().append(sgn*x).reverse().toString();
-		long xRev = sgn*Long.parseLong(numReverseStr);
-		if(xRev<Integer.MIN_VALUE || xRev>Integer.MAX_VALUE) xRev = 0;
+		long xRev = Long.parseLong(numReverseStr);
+		xRev = (xRev>Integer.MAX_VALUE)? 0:sgn*xRev;
+		return (int) xRev;
+	}
+	
+	//Solution2
+	private String StrReverse(String s) {
+		int sLen = s.length();
+		char[] sCharArr = s.toCharArray();
+		char ch;
+		for (int i = 0; i < sLen/2; i++) {
+			ch = sCharArr[i];
+			sCharArr[i] = sCharArr[sLen-i-1];
+			sCharArr[sLen-i-1] = ch;
+		}
+		return new String(sCharArr);
+	}
+	
+	public int reverse2(int x) {
+		if(x==Integer.MIN_VALUE) return 0;
+		int num = (x>0)?x:-1*x;
+		String numReverseStr = StrReverse(String.valueOf(num));
+		long xRev = Long.parseLong(numReverseStr);
+		if(xRev>Integer.MAX_VALUE) return 0;
+		if(x<0) xRev*=-1;
 		return (int) xRev;
 	}
 	
 	
 	public static void main(String[] args) {
 		//int x = 1534236469;
-		int x = -2147483648; //等于Integer.MIN_VALUE 
+		//int x = -2147483648; //等于Integer.MIN_VALUE 
+		int x = -214748;
 		
 		long t1 = System.nanoTime();
-		int xRev = new ReverseInteger2().reverse(x);
+		int xRev = new ReverseInteger2().reverse2(x);
 		long t2 = System.nanoTime();
 		
 		System.out.println("intput: "+x);
