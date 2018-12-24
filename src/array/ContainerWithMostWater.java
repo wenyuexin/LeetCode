2c3ba100582482d5d3624f3b6afe3234a7722783
Apollo4634 <apollo4634@gmail.com>
2018-12-25 00:06:24 +0800

    12250006

---
+++ src/array/ContainerWithMostWater.java
@@ -0,0 +1,15 @@
+package array;
+
+/** 
+ * @author Apollo4634 
+ * @creation 2018/12/25 00:12
+ */
+
+public class ContainerWithMostWater {
+
+	//Solution
+	public int maxArea(int[] height) {
+
+		return 0;
+	}
+}
