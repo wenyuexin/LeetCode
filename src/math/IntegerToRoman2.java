67f15ea2da7f309ef13cb807861ac160c34077c7
Apollo4634 <apollo4634@gmail.com>
2019-01-04 00:06:20 +0800

    01040006

--- src/math/IntegerToRoman2.java
+++ src/math/IntegerToRoman2.java
@@ -1,5 +1,7 @@
 package math;
 
+import array.ContainerWithMostWater2;
+
 /** 
  * @author Apollo4634 
  * @creation 2019/01/01 17:01
@@ -12,50 +14,90 @@ package math;
  * 施工中... 2019-01-02
  * 
  * 参考：
- * I 1
- * V 5
- * X 10
- * L 50
- * C 100
- * D 500
- * M 1000
+ * I 1    V 5    X 10    L 50
+ * C 100  D 500  M 1000
  */
 
 public class IntegerToRoman2 {
 
-	//Solution
-	enum Symbol {I, V, X, L, C, D, M};
-	
+	private static int[] basicValue = new int[] {
+			1,4,5,9,10,40,50,90,100,400,500,900,1000};
+	private static String[] basicSymbolArr = new String[] {
+			"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+
+	//Solution - java11
 	public String intToRoman(int num) {
-		int[] basicValue = new int[] {1,4,5,9,10,40,50,90,100,400,500,900,1000};
-		String[] basicSymbolArr = new String[] {
-				"I","IV","V","IX","X","IL","L","IC","C","ID","D","IM","M"};
-		
 		int residual = num;
 		int nSymbol = 0;
 		StringBuilder sb = new StringBuilder();
-		for (int i = basicSymbolArr.length-1; i>0; i--) {
+		for (int i = basicValue.length-1; i>0; i--) {
 			if(residual>=basicValue[i]) {
 				nSymbol = residual/basicValue[i];
-				sb.append(basicSymbolArr[i]);
-				for (int j = 0; i < nSymbol-1; j++) {
-					sb.append('I');				
-				}
-				residual -= basicValue[i];
-				residual -= (nSymbol-1)*1000; //这里需要修改
+				sb.append(basicSymbolArr[i].repeat(nSymbol));
+				residual -= nSymbol*basicValue[i];
 			}
 		}
-		
 		return sb.toString();
 	}
 
+	//Solution2
+	public String intToRoman2(int num) {
+		int residual = num;
+		int nSymbol = 0;
+		int idx = 12;
+		if(residual<=10) {
+			idx = 4;
+		} else if (residual<=100) {
+			idx = 8;
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = idx; i>=0; i--) {
+			if(residual>=basicValue[i]) {
+				nSymbol = residual/basicValue[i];
+				residual -= nSymbol*basicValue[i];
+				while(nSymbol>0) {
+					sb.append(basicSymbolArr[i]);
+					nSymbol--;
+				}
+			}
+		}
+		return sb.toString();
+	}
+
+	//Solution3
+	public String intToRoman3(int num) {
+		int residual = num;
+		int idx = 12;
+		if(residual<=10) {
+			idx = 4;
+		} else if (residual<=100) {
+			idx = 8;
+		}
+
+		StringBuilder sb = new StringBuilder();
+		while(residual>0) {
+			if(residual>=basicValue[idx]) {
+				residual -= basicValue[idx];
+				sb.append(basicSymbolArr[idx]);
+			} else {
+				idx--;
+			}
+		}
+		return sb.toString();
+	}
 	
+
 	public static void main(String[] args) {
-		
-		char[] arr = new char[5];
-		arr[0] = 'a';
-		arr[1] = 'b';
-		String s = new String(arr);
-		System.out.println(s);
+		//int num = 3999; //MMMCMXCIX
+		int num = 3; //III
+
+		long t1 = System.nanoTime();
+		String romanStr = new IntegerToRoman2().intToRoman3(num);
+		long t2 = System.nanoTime();
+
+		System.out.println("input: "+num);
+		System.out.println("output: "+romanStr);
+		System.out.println("Rumtime: "+(t2-t1)/1.0E6+" ms");
 	}
 }
