d4ab2ef40d0e2853699fdb4c7549c8669e03ae29
Apollo4634 <apollo4634@gmail.com>
2019-01-05 01:33:34 +0800

    01050133

--- solution/math/RomanToInteger2.java
+++ solution/math/RomanToInteger2.java
@@ -9,17 +9,68 @@ package math;
  * 参考：
  * I 1, V 5, X 10, L 50, C 100, D 500, M 1000
  * 
+ * I IV IX, X XL XC, C CD CM, V L D M
+ * 
  * 解题思路：
  * 
  */
 
 public class RomanToInteger2 {
 
-	//Solution
-	public int romanToInt(String s) {
+	/*
+	private static int[] basicValue = new int[] {
+			1,4,5,9,10,40,50,90,100,400,500,900,1000};
+	private static String[] basicSymbolArr = new String[] {
+			"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+	 */
 
-		return 0;
+	//Solution - 施工中
+	public int romanToInt(String s) {
+		char[] sCharArr = s.toCharArray();
+
+		int num = 0;
+		for (int i = 0; i < sCharArr.length; i++) {
+			switch (sCharArr[i]) {
+			case 'I': //I IV IX
+				num += 1;
+				if(i!=sCharArr.length-1) {
+					if(sCharArr[i+1]=='V') { num += 3; i++; continue; }
+					if(sCharArr[i+1]=='X') { num += 8; i++; continue; }
+				}
+				break;
+			case 'V':
+				num += 5;
+				break;
+			case 'X': //X XL XC
+				num += 10;
+				if(i!=sCharArr.length-1) {
+					if(sCharArr[i+1]=='L') { num += 30; i++; continue; }
+					if(sCharArr[i+1]=='C') { num += 80; i++; continue; }
+				}
+				break;
+			case 'L':
+				num += 50;
+				break;
+			case 'C': //C CD CM
+				num += 100;
+				if(i!=sCharArr.length-1) {
+					if(sCharArr[i+1]=='D') { num += 300; i++; continue; }
+					if(sCharArr[i+1]=='M') { num += 800; i++; continue; }
+				}
+				break;
+			case 'D':
+				num += 500;
+				break;
+			case 'M':
+				num += 1000;
+				break;
+			default:
+				break;
+			}
+		}
+		return num;
 	}
+
 
 	public static void main(String[] args) {
 		//输入字符串对应数字的范围是 1 至 3999
