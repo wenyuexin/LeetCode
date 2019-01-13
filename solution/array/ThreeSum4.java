8afd2ff9b54dd8a5cb08aafb8ad4e4bf63637dc6
Apollo4634 <wenyuexin@163.com>
2019-01-13 23:24:50 +0800

    01132324

--- solution/array/ThreeSum4.java
+++ solution/array/ThreeSum4.java
@@ -52,19 +52,19 @@ public class ThreeSum4 {
 		for (int i = 0; i < numsLen; i++) {
 			map.put(nums[i], i);
 		}
-		
-		int sum12;
+
+		int sum12, idx;
 		List<Integer> triplet;
 		for (int i = 0; i < numsLen-2; i++) {
 			if(nums[i]>0) break;
 			for (int j = i+1; j < numsLen-1; j++) {
 				sum12 = nums[i]+nums[j];
-				for (int k = j+1; k < numsLen; k++) {
-					if(nums[k]==-sum12) {
-						triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+				if(map.containsKey(-sum12)) {
+					idx = map.get(-sum12);
+					if(idx!=i && idx!=j) {
+						triplet = Arrays.asList(nums[i], nums[j], -sum12);
 						Collections.sort(triplet);
 						if(!list.contains(triplet)) list.add(triplet);
-						break;
 					}
 				}
 			}
@@ -81,10 +81,13 @@ public class ThreeSum4 {
 		//[[0,0,0]]
 		//int[] nums = {0,0,0};
 
+		//[[0,0,0]]
+		int[] nums = {-1,0,1};
+		
 		//int[] nums = {-2,-3,0,0,-2};
 
 		//[[-4,-2,6],[-4,0,4],[-4,1,3],[-4,2,2],[-2,-2,4],[-2,0,2]]
-		int[] nums = {-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6}; 
+		//int[] nums = {-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6}; 
 
 		//[[-1,-4,5],[-2,-3,5]]
 		//int[] nums = {-1,0,-4,-2,-3,5};
