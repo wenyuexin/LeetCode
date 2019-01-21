1afafbad6ebfba3d4f718b134c9390f1a7133af1
Apollo4634 <apollo4634@gmail.com>
2019-01-21 23:57:27 +0800

    01212357 17

--- solution/string/LetterCombinationsOfAPhoneNumber.java
+++ solution/string/LetterCombinationsOfAPhoneNumber.java
@@ -1,6 +1,7 @@
 package string;
 
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -17,73 +18,57 @@ import java.util.List;
 
 public class LetterCombinationsOfAPhoneNumber {
 	
-	//Solution - 施工中
+	//Solution
 	private static final char[][] mapping = new char[][] {
-		{'a','b','c'}, {'d','e','f'}, {'g','h','i'},
-		{'j','k','l'}, {'m','n','o'}, {'p','q','r','s'},
-		{'t','u','v'}, {'w','x','y','z'}
+		{'a','b','c'}, {'d','e','f'}, {'g','h','i'}, //2(0) 3(1) 4(2)
+		{'j','k','l'}, {'m','n','o'}, {'p','q','r','s'}, //5(3) 6(4) 7(5) 
+		{'t','u','v'}, {'w','x','y','z'} //8(6) 9(7)
 	};
 	
 	private static final int[] lensArr = new int[] {3,3,3,3,3,4,3,4};
-	
-	//private int[] digitsArr;
-	//private int[] state; //字符的组合状态
-	//private StringBuilder sb;
-	
-	/*
-	private static String getStr(int idx) {
-		
-		//更新组合状态
-		String str = "";
-		boolean isLastOne = false;
-		for (int i = 0; i < state.length; i++) {
-			if(state[i]+1==lens[digitsArr[i]]) { //进位
-				
-			} else {
-				
-			}
-		}
-		
-		
-		return str;
-	}
-	*/
 	
 	public List<String> letterCombinations(String digits) {
 		List<String> strList = new LinkedList<>(); //output
 		int nDigits = digits.length();
 		if(nDigits==0) return strList;
 		
-		int[] digitsArr = new int[nDigits];
+		int[] digitsArr = new int[nDigits];//输入字符串转int数组
 		int[] state = new int[nDigits];//字符的组合状态
-		
 		int nStr = 1; //输出字符串的总数
+		StringBuilder sb = new StringBuilder();
 		for (int iDigit = 0; iDigit < nDigits; iDigit++) {
 			digitsArr[iDigit] = digits.charAt(iDigit)-50; //48+2
 			nStr *= lensArr[digitsArr[iDigit]];
+			sb.append(mapping[digitsArr[iDigit]][0]);
 		}
+		//System.out.println(Arrays.toString(digitsArr)+" "+sb);
 		
-		StringBuilder sb = new StringBuilder();
 		int iStr = 0;
+		int carrier = 0;
 		while (iStr<nStr) {
-			for (int iDigit = nDigits-1; iDigit>0; iDigit--) {
-				if(state[iDigit]+1<lensArr[digitsArr[iDigit]]) { 
-					state[iDigit]++;
-					sb.setCharAt(iDigit, mapping[digitsArr[iDigit]][state[iDigit]]);					
+			strList.add(sb.toString());
+			//System.out.print(iStr+" "+Arrays.toString(state)+" ");
+			carrier = 1;
+			for (int iDigit = nDigits-1; iDigit>=0; iDigit--) {
+				if(carrier==0) break; 
+				if(state[iDigit]+carrier<lensArr[digitsArr[iDigit]]) { 
+					state[iDigit] += carrier;
+					sb.setCharAt(iDigit, mapping[digitsArr[iDigit]][state[iDigit]]);
+					carrier = 0;
 				} else {
-					
+					state[iDigit] = 0;
+					sb.setCharAt(iDigit, mapping[digitsArr[iDigit]][0]);		
+					carrier = 1;
 				}
 			}
-			strList.add(sb.toString());
 			iStr++;
 		}
-		
 		return strList;
 	}
 
 	
 	public static void main(String[] args) {
-		String digits = "235";
+		String digits = "23";
 		
 		long t1 = System.nanoTime();
 		LetterCombinationsOfAPhoneNumber obj = 
@@ -92,7 +77,7 @@ public class LetterCombinationsOfAPhoneNumber {
 		long t2 = System.nanoTime();
 
 		System.out.println("input:   "+digits);
-		System.out.println("output:  "+strList);
+		System.out.println("output:  "+strList.size()+" "+strList);
 		System.out.println("Rumtime: "+(t2-t1)/1.0E6+" ms");
 	}
 }
