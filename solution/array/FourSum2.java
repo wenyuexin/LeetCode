5bfba4053ecf4945f682064da1ca275b1dd03312
Apollo4634 <43531404+Apollo4634@users.noreply.github.com>
2019-01-25 10:28:37 +0800

    Update FourSum2.java

--- solution/array/FourSum2.java
+++ solution/array/FourSum2.java
@@ -7,6 +7,9 @@ import java.util.List;
 /** 
  * @author Apollo4634 
  * @creation 2019/01/22 23:01
+ * 
+ * 18 four sum
+ * 
  */
 
 public class FourSum2 {
@@ -19,21 +22,22 @@ public class FourSum2 {
 		
 		int sum12, sum34, i3, i4;
 		for (int i1 = 0; i1 < numsLen-3; i1++) {
-			if(nums[i1]>0) break;
+			//if(nums[i1]>0) break;
 			for (int i2 = i1+1; i2 < numsLen-2; i2++) {
 				sum12 = nums[i1]+nums[i2];
-				if(sum12>0) break;
+				//if(sum12>0) break;
 				
 				i3 = i2+1;
 				i4 = numsLen-1;
 				while (i3<i4) {
 					sum34 = nums[i3]+nums[i4];
 					while(sum12+sum34<target) i3++;
-					while(sum12+sum34>target) i3--;
+					while(sum12+sum34>target) i4--;
 					
 					if(sum12+sum34==target) {
 						list.add(Arrays.asList(
 							nums[i1], nums[i2], nums[i3], nums[i4]));
+						i3++；
 					}
 				}
 				while (i2<numsLen-1 && nums[i2]==nums[i2+1]) i2++;
