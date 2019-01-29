e6e7b375a4b03f700347b0c46e2d91ba98438f04
Apollo4634 <apollo4634@gmail.com>
2019-01-29 17:52:03 +0800

    01291751 20

--- solution/linked_list/AddTwoNumbers.java
+++ solution/linked_list/AddTwoNumbers.java
@@ -9,57 +9,8 @@ package linked_list;
  */
 
 public class AddTwoNumbers {
-	//设置链表 - 方法1
-	static ListNode set(int[] arr) {
-		ListNode listNode = new ListNode(arr[0]);//由于java对象引用的特点，有必要保留表头
-		ListNode listNode_tmp = listNode;
-		ListNode nextNode = null;
-		
-		for(int i=1; i<arr.length; i++) {
-			nextNode = new ListNode(arr[i]);
-			listNode_tmp.next = nextNode;
-			listNode_tmp = nextNode;
-		}
-		
-		return listNode;
-	}
 	
-	//设置链表 - 方法2 （此法繁琐）
-	static ListNode set2(int[] arr) {
-		ListNode listNode = new ListNode(arr[0]);
-		ListNode nextNode = new ListNode(0);
-		
-		if(arr.length == 1) {
-			listNode.next = null;
-		} else {
-			listNode.next = nextNode;
-			int arrLen = arr.length;
-			for(int i=1; i<arrLen; i++) {
-				nextNode.val = arr[i];
-				
-				if(i != arrLen-1) {
-					nextNode.next = new ListNode(0);
-				} else {
-					nextNode.next = null;
-				}
-				nextNode = nextNode.next;
-			}
-		}
-		return listNode;
-	}
-	
-	//打印链表
-	static void print(ListNode listNode) {
-		ListNode node = listNode;
-		while(node != null) {
-			System.out.print(node.val + " ");
-			node = node.next;
-		}
-		System.out.println();
-	}
-
-	
-	/******************* Answer *******************/
+	//Solution
 	//某一位上的加法计算
 	static int[] calc(int val1, int val2, int c) {
 		int[] out = new int[] {0,0}; //m,r
@@ -97,43 +48,24 @@ public class AddTwoNumbers {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("===== test =====");
-		//这种方式赋值后，其实链表中有两个元素
-		//只不过第二个元素的next是null
-		ListNode node = new ListNode(0);
-		ListNode node2 = new ListNode(0);
-		node.next = node2;
-		node2 = null; //注意，这里将node2设为null，并不代表node的next变成了null
-		              //实际上只是node2引用的对象由之前new ListNode(0)生成的对象变成了null
-		              //node的next本质上还是指向之前的new ListNode(0)
-		
-		ListNode node3 = null;
-		ListNode node3b = new ListNode(3); 
-		node3b.next = node3;
-		
-		ListNode node4 = new ListNode(0);
-		node4 = null;
-		ListNode node4b = new ListNode(2);
-		node4b.next = node4;
-		
-		System.out.println("===== Solution =====");
 		int[] arr1 = new int[] {5,8};
 		int[] arr2 = new int[] {5,4};
 		
-		ListNode l1 = AddTwoNumbers.set(arr1);
-		ListNode l2 = AddTwoNumbers.set(arr2); 
-		System.out.print("ListNode1: ");
-		AddTwoNumbers.print(l1);	
-		System.out.print("ListNode2: ");
-		AddTwoNumbers.print(l2);
+		ListNode l1 = ListNode.setNodeList(arr1);
+		ListNode l2 = ListNode.setNodeList(arr2); 
+		
+		System.out.println("input list1: ["+l1.listToString()+"]");
+		System.out.println("input list2: ["+l2.listToString()+"]");
 		System.out.println("=====");
 		
-		ListNode l3 = new AddTwoNumbers().addTwoNumbers(l1, l2);
-		System.out.print("ListNode1: ");
-		AddTwoNumbers.print(l1);	
-		System.out.print("ListNode2: ");
-		AddTwoNumbers.print(l2);
-		System.out.print("ListNode3: ");
-		AddTwoNumbers.print(l3);
+		long t1 = System.nanoTime();
+		AddTwoNumbers obj = new AddTwoNumbers();
+		ListNode l3 = obj.addTwoNumbers(l1, l2);
+		long t2 = System.nanoTime();
+		
+		System.out.println("input list1:  ["+l1.listToString()+"]");
+		System.out.println("input list2:  ["+l2.listToString()+"]");
+		System.out.println("output list3: ["+l3.listToString()+"]");
+		System.out.println("Rumtime: "+(t2-t1)/1.0E6+" ms");
 	}
 }
--- solution/linked_list/RemoveNthNodeFromEndOfList.java
+++ solution/linked_list/RemoveNthNodeFromEndOfList.java
@@ -5,7 +5,7 @@ package linked_list;
  * 
  * 
  * @author Apollo4634
- * @date 2019/01/28 20:49
+ * @date 2019/01/28
  * @problem 20
  * @see ListNode
  */
@@ -14,14 +14,44 @@ public class RemoveNthNodeFromEndOfList {
 
 	//Solution
 	public ListNode removeNthFromEnd(ListNode head, int n) {
-		//ListNode node;
-
-
+		if(head==null) return head;
+		int idx = 0;
+		ListNode node = head;
+		ListNode tmpNode = head;
+		
+		while(node.next!=null) {
+			if(idx>n) tmpNode = tmpNode.next;
+			node = node.next; idx++;
+		}
+		if(idx>n) tmpNode = tmpNode.next;
+		
+		if(idx>=n) {
+			tmpNode.next = tmpNode.next.next;
+		} else {
+			head = head.next;
+		}
 		return head;
 	}
 
 
 	public static void main(String[] args) {
+		//int[] nums = new int[] { 7,6,5,4,3,2,1 };
+		//int n = 7;
+		
+		int[] nums = new int[] { 1 };
+		int n = 1;
 
+		ListNode head = ListNode.setNodeList(nums);
+		System.out.println("input list:  "+ListNode.listToString(head));
+		System.out.println("input n:     "+n);
+		
+		long t1 = System.nanoTime();
+		RemoveNthNodeFromEndOfList obj = 
+				new RemoveNthNodeFromEndOfList();
+		ListNode out = obj.removeNthFromEnd(head, n);
+		long t2 = System.nanoTime();
+		
+		System.out.println("output list: "+ListNode.listToString(out));
+		System.out.println("Rumtime: "+(t2-t1)/1.0E6+" ms");
 	}
 }
