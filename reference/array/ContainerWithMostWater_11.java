df8a98c85c46385c12f0d33cf3269cec6938663d
Apollo4634 <wenyuexin@163.com>
2019-01-31 22:22:10 +0800

    01312222

--- reference/array/ContainerWithMostWater_11.java
+++ reference/array/ContainerWithMostWater_11.java
@@ -1,7 +1,13 @@
 package array;
 
 /**
- * @author Apollo4634
+ * leetcode url:
+ * https://leetcode.com/problems/container-with-most-water/solution/
+ * 
+ * Approach 1: Brute Force
+ * Approach 2: Two Pointer Approach - Recommend
+ * 
+ * @author -
  * @date 2019/01/30
  * @problem 11
  */
@@ -11,12 +17,8 @@ public class ContainerWithMostWater_11 {
 	/**
 	 * Approach 1: Brute Force
 	 * 
-	 * In this case, we will simply consider the area for 
-	 * every possible pair of the lines and 
-	 * find out the maximum area out of those.
-	 * 
-	 * Time complexity : O(n^2). Calculating area for all [n(n-1)]/2 height pairs.
-	 * Space complexity : O(1). Constant extra space is used. 
+	 * Time complexity : O(n^2).
+	 * Space complexity : O(1).
 	 */
 	public int maxArea(int[] height) {
 		int maxarea = 0;
@@ -26,24 +28,42 @@ public class ContainerWithMostWater_11 {
 		return maxarea;
 	}
 
+	
 	/**
 	 * Approach 2: Two Pointer Approach
-	 * The intuition behind this approach is that the area formed between the lines will always be limited by 
-	 * the height of the shorter line. Further, the farther the lines, the more will be the area obtained.
 	 * 
-	 * We take two pointers, one at the beginning and one at the end of the array constituting the length of the lines. 
-	 * Futher, we maintain a variable maxareamaxarea to store the maximum area obtained till now. 
-	 * At every step, we find out the area formed between them, 
-	 * update maxareamaxarea and move the pointer pointing to the shorter line towards the other end by one step.
+	 * Algorithm
+	 * The intuition behind this approach is that the area formed between the lines will 
+	 * always be limited by the height of the shorter line. Further, the farther the lines, 
+	 * the more will be the area obtained.
 	 * 
-	 * The algorithm can be better understood by looking at the example below:
-	 * 1 8 6 2 5 4 8 3 7
-	 * @see <a href="https://leetcode.com/problems/container-with-most-water/solution/">leetcode</a>
+	 * We take two pointers, one at the beginning and one at the end of the array 
+	 * constituting the length of the lines. Futher, we maintain a variable maxareamaxarea 
+	 * to store the maximum area obtained till now. At every step, we find out the area 
+	 * formed between them, update maxareamaxarea and move the pointer pointing to 
+	 * the shorter line towards the other end by one step.
+	 * 
+	 * Proof:
+	 * Suppose we get n numbers Q1, Q2, ... , Qn-1, Qn.
+	 * then we define the set Sk to contains k (the length) continued numbers
+	 * (e.g. {Q1, Q2, ... Qk} or {Q3, Q4, ... , Qk+2} ...), 
+	 * define Ski be the set start with Qi -- {Qi, Qi+1, ... , Qk+i-1} and 
+	 * then the area of Ski (let it be A(Ski) ) is (min(Qi, Qk+i-1) * k).
+	 * Suppose we start with Ski, when we minus one item, 
+	 * we get two subsets S(k-1)i and S(k-1)(i+1), 
+	 * if Qi < Qk+i-1, we can see A(S(k-1)i)=min(Qi, Qk+i-2)(k-1)< A(Ski)=Qik, 
+	 * so we can determine any set contains Qi and length is smaller than k 
+	 * will not be the candidate that area will surpass the prior max area. 
+	 * So the candidates sets can only be found in the other subset 
+	 * S(k-1)(i+1)--{Qi+1, Qi+2, ... , Qi+k-1}. 
+	 * 
+	 * Time complexity : O(n).
+	 * Space complexity : O(1).
 	 */
 	public int maxArea2(int[] height) {
 		int maxarea = 0, l = 0, r = height.length - 1;
 		while (l < r) {
-			maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+			maxarea = Math.max(maxarea, Math.min(height[l],height[r])*(r-l));
 			if (height[l] < height[r])
 				l++;
 			else
@@ -51,36 +71,4 @@ public class ContainerWithMostWater_11 {
 		}
 		return maxarea;
 	}
-
-
-	/**
-	 * Approach 3: 3ms version
-	 */
-	public int maxArea3(int[] height) {
-		int smax = 0;
-		int l = 0;
-		int r = height.length-1;
-		int hl = height[l];
-		int hr = height[r];
-		while(l < r) {
-			int minh = hl < hr ? hl : hr;
-			int curs = minh * (r - l);
-			if (curs > smax) {
-				smax = curs;
-			}
-			if (minh == hl) {
-				while(minh >= hl && l < r) {
-					++l;
-					hl = height[l];
-				}
-			} else {
-				while(minh >= hr && l < r) {    
-					--r;
-					hr = height[r];
-				}
-			}
-		}
-		return smax;
-	}
-
 }
--- solution/array/ThreeSumClosest.java
+++ solution/array/ThreeSumClosest.java
@@ -2,119 +2,104 @@ package array;
 
 import java.util.Arrays;
 
-/** 
- * 总体思路可以参考 No.15 Three Sum，都是先排序后遍历，
- * 区别在于15题需要即使找到了符合要求的三元组仍然需要继续遍历，
- * 而本题的目标是寻找sum值最接近target的三元组，
- * 有时候则可以使用continue或者break直接跳过循环，
- * 如果刚好等于target可以直接return
- * （具体可以参考class ThreeSum中的threeSum3方法）
- * 
- * 这里补充几点：
- * 假设三元组{e1,e2,e3}元素的下标分别是i j k，三者满足i<j<k
- * 假设numsLen=nums.length-1
- * 假设和target最接近的三元组的sum值为closestSum
- * 假设idx(e)表示元素e在数组nums中的下标
- * 
- * (A) 
- * 对于固定的i和j，对k进行循环遍历的目标是，
- * 在[j+1, rightIdx]区间内寻找是否存在某个值等e3=target-e1-e2
- * （这里，rightIdx是k的右界，初始状态为numsLen-1，
- * 后续遍历过程中视情况有可能减小，从而缩小搜索范围）
- * 
- * 遍历过程中，根据nums[k]和e3的大小关系，可以分为2种情况：
- * a.若nums[k]==e3，那么直接return target
- * b.若nums[k]!=e3，即数组nums中存在序列 
- * ... nums[k'-1], nums[k'] (e3) nums[k'+1] ...
- * 那么对k遍历时，差值diff = nums[k']-e3会存在一次变号，
- * 可以判断(e1+e2+nums[k'])和(e1+e2+nums[k'+1])之间哪个数更接近target
- * 
- * (B)关于A-b中的判断
- * 在实际上不一定同时存在比e3小的nums[k']和比e3大的nums[k'+1]，例如：
- * a. k'+1>rightIdx时自然就没有nums[k'+1]
- * b. 如果nums[j+1]>e3，即nums[j+1]至nums[rightIdx]之间的数都大于e3,
- * 那么就不存在比e3小的nums[k']，此时需要计算k=j+1时的sum值，
- * 即sum=nums[i]+nums[j]+nums[j+1]，然后判断是否可以更新closestSum
- * 
- * (C)关于rightIdx
- * k的范围，初步的想法是[j+1, numsLen-1]，即k的左界和上一个三元组有关，
- * 但是，分析发现在固定e1(i不变)的时候，k的右界也和上一个三元组有关
- * 具体如下：
- * 对于固定的e1(即i不变)，将e2更新为e2'(即j=j+1)时，e2变大(e2'>e2)，
- * 如果仍然要满足e1+e2+e3=target，那么e3的值必然要变小。
- * 因此，对于上一组e1 e2，如果存在nums[k']<e3<nums[k'+1]，
- * 那么e3的搜索范围收缩为[j+1, k']，也就是说rightIdx=k'
- * 
- * (D)关于直接i++和j++
- * a. 对于某个e1(下标为i)，如果nums[i]==nums[i+1]==e1，那么，
- * 以nums[i+1]为e1得到的三元组的集合包含了以nums[i]为e1得到的三元组的集合.
- * 依次类推，如果nums[i]==nums[i+1]==...==nums[i+I]，那么
- * 以nums[i]为e1得到的三元组的集合也包含了以nums[i+I]为e1得到的三元组的集合，
- * 因此，nums[i]为e1搜索后不需要再以nums[i+i'] (i'=1,2,...,I)为e1进行搜索.
- * b. 同理，对于固定的e1，如果nums[j]==nums[j+1]==...==nums[j+J]==e2，
- * 那么以nums[j]为e2得到的三元组的集合也包含了以nums[j+J]为e2得到的三元组的集合.
- * 
- * @author Apollo4634 
- * @date 2019/01/15 23:01
+/**
+ * @author Apollo4634
+ * @date 2019/01/31
  * @problem 16
  */
 
 public class ThreeSumClosest {
-
-	//Solution1
+	
+	//Solution
+	private int[] calcSearchRange(int[] nums, int target) {
+		int numsLen = nums.length;
+		int I1=0, i2, i3, e2e3, diff;
+		int minAbsDiff = Integer.MAX_VALUE;
+		int tmp = nums[numsLen-2]+nums[numsLen-1]-target;
+		for (int i = 0; i < numsLen-2; i++) {
+			diff = nums[i]+tmp;
+			if(diff<0) { minAbsDiff=diff; } 
+			else { I1=(i>0)?i:0; break; }
+		}
+		
+		int I3=numsLen-1;
+		int tmp2 = nums[0]+nums[1]-target;
+		for (int i = 0; i < numsLen-2; i++) {
+			diff = nums[i]+tmp2;
+			if(diff>0) { minAbsDiff=diff; } 
+			else { I1=(i>0)?i:0; break; }
+		}
+		
+		return new int[] {I1,I3,minAbsDiff};
+	}
+			
 	public int threeSumClosest(int[] nums, int target) {
-		int closestSum = 0;
 		int numsLen = nums.length;
 		if(numsLen<4) {
-			for (int num: nums) { closestSum += num; }
+			int closestSum = 0;
+			for (int num: nums) closestSum += num;
 			return closestSum;
 		}
 		Arrays.sort(nums); //排序
 
-		int rightIdx, diff;
-		int closestDiff = Integer.MAX_VALUE;
-		for (int i = 0; i < numsLen-2; i++) { //e1
-			rightIdx = numsLen-1;
-			for (int j = i+1; j < numsLen-1; j++) { //e2
-				int e3 = target - nums[i] - nums[j];
-				for (int k = rightIdx; k > j; k--) { //e3
-					if(nums[k]==e3) return target;
-					if(k!=j+1 && k+1<=rightIdx && nums[k]==nums[k+1]) continue;
-					
-					if(nums[k]<e3 || k==j+1) {
-						diff = nums[k]-e3;
-						if(k+1<numsLen && (nums[k+1]-e3<-diff)) {diff = nums[k+1]-e3;}
-						if(Math.abs(diff)<Math.abs(closestDiff)) closestDiff = diff;
-						rightIdx = k; //更新k的右界
-						break;
-					}
-				}
-				while(j<numsLen-1 && nums[j+1]==nums[j]) j++;
-			}
-			while(i<numsLen-2 && nums[i+1]==nums[i]) i++;
+		int I1=0, i2, i3, e2e3, diff;
+		int minAbsDiff = Integer.MAX_VALUE;
+		int tmp = nums[numsLen-2]+nums[numsLen-1]-target;
+		for (int i = 0; i < numsLen-2; i++) {
+			diff = nums[i]+tmp;
+			if(diff<0) { minAbsDiff=diff; } 
+			else { I1=(i>0)?i:0; break; }
 		}
-		return target + closestDiff;
+		
+		for (int i1 = I1; i1 < numsLen-2; i1++) {
+			i2 = i1+1;
+			i3 = numsLen-1;
+			e2e3 = target-nums[i1];
+			
+			diff = nums[i2]+nums[i2+1]-e2e3;
+			if(diff>=0 && Math.abs(diff)<Math.abs(minAbsDiff)) {
+				minAbsDiff=diff; break;
+			}
+			diff = nums[i3]+nums[i3-1]-e2e3;
+			if(diff<0 && Math.abs(diff)<Math.abs(minAbsDiff)) {
+				minAbsDiff=diff; continue;
+			}
+			
+			while(i2<i3) { //twoSumClosest
+				diff = nums[i2] + nums[i3] - e2e3;
+				if(diff == 0) return target;
+				if(Math.abs(diff)<Math.abs(minAbsDiff)) minAbsDiff=diff;
+				
+				if(diff<0) { i2++; while(i2<i3 && nums[i2]==nums[i2-1]) i2++; } 
+				else { i3--; while(i2<i3 && nums[i3]==nums[i3+1]) i3--; }
+			}
+			while(i1<numsLen-2 && nums[i1+1]==nums[i1]) i1++;
+		}
+		return target + minAbsDiff;
 	}
-
-
+	
+	
 	public static void main(String[] args) {
 		//int[] nums = {-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0};
 		//int target = 9;
 
 		//int[] nums = {-1,2,1,-4};
 		//int target = 1;
+
+		//int[] nums = {1,1,1,1};
+		//int target = 1;
 		
-		int[] nums = {1,1,1,1};
-		int target = 1;
+		int[] nums = { -1,0,1,1,55 };
+		int target = 3;
 
 		long t1 = System.nanoTime();
 		ThreeSumClosest obj = new ThreeSumClosest();
 		int closestSum = obj.threeSumClosest(nums, target);
 		long t2 = System.nanoTime();
 
-		System.out.println("input:  array  = "+Arrays.toString(nums));
-		System.out.println("input:  target = "+target);
-		System.out.println("output:  "+closestSum);
+		System.out.println("input: array  = "+Arrays.toString(nums));
+		System.out.println("input: target = "+target);
+		System.out.println("output: "+closestSum);
 		System.out.println("Rumtime: "+(t2-t1)/1.0E6+" ms");
 	}
 }
