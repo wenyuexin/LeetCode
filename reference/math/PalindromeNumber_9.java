0f56a700d4e42a52db8fc532f0a5ae0f8cdb0edb
Apollo4634 <apollo4634@gmail.com>
2019-02-01 14:50:19 +0800

    02011450

---
+++ reference/math/PalindromeNumber_9.java
@@ -0,0 +1,52 @@
+package math;
+
+/**
+ * @author -
+ * @date 2019/02/01
+ * @problem 9
+ */
+
+public class PalindromeNumber_9 {
+
+	/*Approach 1: A submission on leetcode*/
+	public boolean isPalindrome(int x) {
+        if(x < 0) return false;
+        if(reverse(x) == x) return true;
+        return false;
+    }
+    
+    private int reverse(int n) {
+        int reverse = 0;
+        while(n!=0){
+            reverse = reverse * 10 + (n %10);
+            n/=10;
+        }
+        return reverse;
+    }
+    
+    
+    /*Approach 2: My solution*/
+    public boolean isPalindrome2(int x) {
+		if(x<0) return false;//负数
+		if(x<10) return true;//个位数
+		if(x%10==0) return false;//个位为0
+
+		int nChar = (int) Math.log10(x)+1;
+		if(nChar==2) return x%10==x/10;
+		if(nChar==10 && x%10>2) return false;
+		
+		int nChar_h1 = (int) (nChar/2.0+0.5);
+		int tenPow = (int) Math.pow(10, nChar_h1);
+		int n1 = x/tenPow; //前半段
+		if(nChar_h1!=nChar/2) tenPow/=10;
+		int n2 = x%tenPow; //后半段
+
+		int n1Rev = 0;
+		while(n1>0) {
+			n1Rev = n1Rev*10 + n1%10;
+			n1 /= 10;
+		}
+		return n1Rev==n2;
+	}
+    
+}
--- solution/math/PalindromeNumber.java
+++ solution/math/PalindromeNumber.java
@@ -1,11 +1,6 @@
 package math;
 
 /** 
- * @author Apollo4634 
- * @creation 2018/12/17 22:12
- * 
- * No.9 Palindrome Number
- * 
  * 解题思路：
  * 先将输入的数字x按中轴线拆分为前半段n1和后半段n2：
  * 例如，x=123321，则n1=123，n2=321
@@ -13,6 +8,10 @@ package math;
  * 然后将n2按位进行反转得到n2Rev，最后返回 (n1==n2Rev)
  * 
  * 关于整数的反转可以参考 No.7 Reverse Integer
+ * 
+ * @author Apollo4634 
+ * @creation 2018/12/17 22:12
+ * @problem 9
  */
 
 public class PalindromeNumber {
@@ -39,31 +38,28 @@ public class PalindromeNumber {
 		return n1==n2Rev;
 	}
 	
-	//Solution2 - Recommendation
+	//Solution2 - Recommend
 	public boolean isPalindrome2(int x) {
-		if(x<0) return false;
-		if(x<10) return true;
-		if(x%10==0) return false;
+		if(x<0) return false;//负数
+		if(x<10) return true;//个位数
+		if(x%10==0) return false;//个位为0
 
 		int nChar = (int) Math.log10(x)+1;
 		if(nChar==2) return x%10==x/10;
 		if(nChar==10 && x%10>2) return false;
 		
 		int nChar_h1 = (int) (nChar/2.0+0.5);
-		int nChar_h2 = nChar/2;
-		int tenPow_1 = (int) Math.pow(10, nChar_h1);
-		int tenPow_2 = (nChar_h1==nChar_h2)?tenPow_1:tenPow_1/10;
-		int n1 = x/tenPow_1;
-		int n2 = x%tenPow_2;
+		int tenPow = (int) Math.pow(10, nChar_h1);
+		int n1 = x/tenPow; //前半段
+		if(nChar_h1!=nChar/2) tenPow/=10;
+		int n2 = x%tenPow; //后半段
 
-		int digit = 0;
-		int n2Rev = 0;
-		for (int i = 0; i < nChar_h2; i++) {
-			digit = n2%10;
-			n2 /= 10;
-			n2Rev = n2Rev*10 + digit;
+		int n1Rev = 0;
+		while(n1>0) {
+			n1Rev = n1Rev*10 + n1%10;
+			n1 /= 10;
 		}
-		return n1==n2Rev;
+		return n1Rev==n2;
 	}
 		
 	//Solution3
@@ -99,19 +95,21 @@ public class PalindromeNumber {
 	
 
 	public static void main(String[] args) {
-		//int x = -121;
+		//int x = 121;
 		//int x = -121;
 		//int x = 12321;
 		//int x = 123321;
 		//int x = 120030221;		
-		int x = 1000030001;		
+		//int x = 1000030001;
+		int x = 1001;
 
 		long t1 = System.nanoTime();
-		boolean flag = new PalindromeNumber().isPalindrome2(x);
+		PalindromeNumber obj = new PalindromeNumber();
+		boolean flag = obj.isPalindrome2(x);
 		long t2 = System.nanoTime();
 
-		System.out.println("input:  "+x);
-		System.out.println("output: "+flag);
-		System.out.println("time: "+(t2-t1)/1.0E6+" ms");
+		System.out.println("Input:  "+x);
+		System.out.println("Output: "+flag);
+		System.out.println("Runtime: "+(t2-t1)/1.0E6+" ms");
 	}
 }
