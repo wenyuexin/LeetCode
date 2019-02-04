30e636a61154a9e531ffbb5c0f6cc17bbfad6f49
Apollo4634 <apollo4634@gmail.com>
2019-02-04 21:43:21 +0800

    02042143

--- reference/linked_list/MergeTwoSortedLists_21.java
+++ reference/linked_list/MergeTwoSortedLists_21.java
@@ -16,27 +16,19 @@ public class MergeTwoSortedLists_21 {
 
 		ListNode node1 = l1;
 		ListNode node2 = l2;
-		while(node1!=null && node2!=null) {
-			if(node1.val<node2.val) {
+		while (node1!=null && node2!=null) {
+			if (node1.val<=node2.val) {
 				node3.next = node1;
 				node1 = node1.next;
-				node3 = node3.next;
-			} else if(node1.val>node2.val) {
-				node3.next = node2;
-				node2 = node2.next;
 				node3 = node3.next;
 			} else {
-				node3.next = node1;
-				node1 = node1.next;
-				node3 = node3.next;
 				node3.next = node2;
 				node2 = node2.next;
 				node3 = node3.next;
 			}
 		}
-
 		if(node1!=null) node3.next = node1;
 		if(node2!=null) node3.next = node2;
 		return l3.next;
-	}	
+	}
 }
