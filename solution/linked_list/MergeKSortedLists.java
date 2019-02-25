d845d5a3e3acb62b600df8071571f909913aa2d0
Apollo4634 <wenyuexin@163.com>
2019-02-26 00:29:37 +0800

    02260029

--- solution/linked_list/MergeKSortedLists.java
+++ solution/linked_list/MergeKSortedLists.java
@@ -1,12 +1,42 @@
 package linked_list;
 
 /** 
+ * 使用最小堆即可解出此题
+ * 
  * @author Apollo4634 
  * @create 2019/02/25
  * @problem 23
+ * @see ListNode
  * @see MergeKSortedLists_23
  */
 
 public class MergeKSortedLists {
+
+	private ListNode[] heap;
+	private int K;
+
+	private void sink() {
+		
+	}
+
+	private void insert(ListNode node) {
+		
+	}
 	
+	private ListNode deleteMin() {
+
+		return null;
+	}
+
+	public ListNode mergeKLists(ListNode[] lists) {
+		K = lists.length;
+		heap = new ListNode[K+2];
+		for (int i = 0; i < K; i++) {
+			heap[i+1] = lists[i];
+		}
+		
+		
+		return null;
+	}
+
 }
