6db034d9e029d87ff902f6b70f6cea680819711a
Apollo4634 <wenyuexin@163.com>
2019-03-01 23:39:24 +0800

    03012339

--- solution/linked_list/SwapNodesInPairs.java
+++ solution/linked_list/SwapNodesInPairs.java
@@ -10,11 +10,18 @@ public class SwapNodesInPairs {
 	public ListNode swapPairs(ListNode head) {
 		if (head == null || head.next == null) return head;
 		ListNode first = new ListNode(0);
+		first.next = head.next;
+		ListNode prev = first;
 		ListNode node1 = head;
 		ListNode node2 = head.next;
 		
 		while (node2 != null) {
-			ListNode temp = node1;
+			node1.next = node2.next;
+			prev.next = node2;
+			node2.next = node1;
+			prev = node1;
+			node1 = prev.next;
+			node2 = prev.next.next;
 			
 		}
 		
