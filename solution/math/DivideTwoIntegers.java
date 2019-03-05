1a6202c9f18666dc4dbaa7a837f919857f48b6b2
Apollo4634 <wenyuexin@163.com>
2019-03-06 00:31:25 +0800

    03060030 29

---
+++ solution/math/DivideTwoIntegers.java
@@ -0,0 +1,25 @@
+package math;
+
+/** 
+ * @author Apollo4634 
+ * @create 2019/03/05
+ * @problem 29
+ */
+
+public class DivideTwoIntegers {
+	public int divide(int dividend, int divisor) {
+		boolean isNegative1 = dividend<0;
+		boolean isNegative2 = divisor<0;
+		
+		if (isNegative1 != isNegative2) {
+			
+		}
+		
+		return 0;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
