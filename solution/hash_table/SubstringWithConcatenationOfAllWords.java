a25f7edbde89c46ef672b3567ac57f2cdcab0552
Apollo4634 <apollo4634@gmail.com>
2019-03-11 23:54:57 +0800

    03112354 problem 30

---
+++ solution/hash_table/SubstringWithConcatenationOfAllWords.java
@@ -0,0 +1,18 @@
+package hash_table;
+
+import java.util.List;
+
+/** 
+ * @author Apollo4634 
+ * @create 2019/03/11
+ * @problem 30
+ * @see SubstringWithConcatenationOfAllWords_30
+ */
+
+public class SubstringWithConcatenationOfAllWords {
+	
+	public List<Integer> findSubstring(String s, String[] words) {
+
+		return null;
+	}
+}
