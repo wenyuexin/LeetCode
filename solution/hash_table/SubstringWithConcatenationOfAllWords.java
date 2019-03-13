32f1276f27552891ac9151e2087fd1641ed73fce
Apollo4634 <apollo4634@gmail.com>
2019-03-14 00:34:37 +0800

    03140034

--- solution/hash_table/SubstringWithConcatenationOfAllWords.java
+++ solution/hash_table/SubstringWithConcatenationOfAllWords.java
@@ -1,8 +1,13 @@
 package hash_table;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /** 
+ * 施工中 ...
+ * 
  * @author Apollo4634 
  * @create 2019/03/11
  * @problem 30
@@ -16,7 +21,11 @@ public class SubstringWithConcatenationOfAllWords {
 		int wordLen = words[0].length();
 		if (s.length() < wordLen) return null;
 		
-		
+		Set<String> set = new HashSet<>(Arrays.asList(words));
+		for (int i = 0; i < words.length-wordLen; ) {
+			if (set.contains(s.substring(i, wordLen))) i += wordLen;
+			else i += 1;
+		}
 		
 		
 		return null;
