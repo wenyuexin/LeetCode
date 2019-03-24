1c0a2bef3cd28f59554932c5c365145a4039d171
Apollo4634 <apollo4634@gmail.com>
2019-03-24 23:59:01 +0800

    0324-2358 problem 133

---
+++ solution/depth_first_search/CloneGraph.java
@@ -0,0 +1,17 @@
+package depth_first_search;
+
+import graph.Node;
+
+/** 
+ * @author Apollo4634 
+ * @create 2019/03/24
+ * @problem 133
+ * @see CloneGraph_133
+ */
+
+public class CloneGraph {
+	public Node cloneGraph(Node node) {
+        
+		return null;
+    }
+}
---
+++ solution/graph/Node.java
@@ -0,0 +1,22 @@
+package graph;
+
+import java.util.List;
+
+/** 
+ * Definition for a Node
+ * 
+ * @author Apollo4634 
+ * @create 2019/03/24
+ */
+
+public class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
