43c4c58792818bf648dfc07b1c49e2d5ebea8108
Apollo4634 <wenyuexin@163.com>
2019-03-26 21:54:48 +0800

    0326-2154 problem133 CloneGraph

--- solution/depth_first_search/CloneGraph.java
+++ solution/depth_first_search/CloneGraph.java
@@ -1,5 +1,12 @@
 package depth_first_search;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
 import graph.Node;
 
 /** 
@@ -10,8 +17,29 @@ import graph.Node;
  */
 
 public class CloneGraph {
+	
 	public Node cloneGraph(Node node) {
+        Map<Node, Boolean> marked = new HashMap<>();
+        Queue<Node> queue = new LinkedList<>();
         
-		return null;
+		Node dummy = new Node(node.val, null);
+		
+        marked.put(node, true);
+		for (Node w: node.neighbors) {
+			if (marked.containsKey(w)) continue;
+			marked.put(w, true);
+			//todo 复制w
+			List<Node> neighbors = new ArrayList<>(w.neighbors.size());
+		}
+        
+        
+		return dummy;
     }
+	
+	public Node cloneNode(Node origin) {
+		if (origin == null) return null;
+		Node copy = new Node(origin.val, null);
+		return copy;
+	}
+	
 }
