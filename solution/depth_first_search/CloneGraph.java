13e459dac1b69b550f00005f76dcd3b88e00b5d1
Apollo4634 <apollo4634@gmail.com>
2019-03-27 01:08:19 +0800

    0327-0108 problem 133

--- solution/depth_first_search/CloneGraph.java
+++ solution/depth_first_search/CloneGraph.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
+import array.ContainerWithMostWater;
 import graph.Node;
 
 /** 
@@ -18,28 +19,50 @@ import graph.Node;
 
 public class CloneGraph {
 	
+	Map<Node, Node> marked;
+	Queue<Node> queue;
+
 	public Node cloneGraph(Node node) {
-        Map<Node, Boolean> marked = new HashMap<>();
-        Queue<Node> queue = new LinkedList<>();
-        
-		Node dummy = new Node(node.val, null);
+		marked = new HashMap<>();
+		queue = new LinkedList<>();
 		
-        marked.put(node, true);
-		for (Node w: node.neighbors) {
-			if (marked.containsKey(w)) continue;
-			marked.put(w, true);
-			//todo 复制w
-			List<Node> neighbors = new ArrayList<>(w.neighbors.size());
-		}
-        
-        
+		Node dummy = new Node(node.val, null);
+        marked.put(node, dummy);
+        visit(node);
 		return dummy;
     }
 	
-	public Node cloneNode(Node origin) {
+	private void visit(Node node) {
+		for (Node w: node.neighbors) {
+			if (marked.containsKey(w)) continue;
+			marked.put(w, node);
+			
+			Node copy = cloneNode(w);
+			copy.neighbors.add(marked.get(node));
+			visit(w);
+			
+			
+		}
+	}
+	
+	private static Node cloneNode(Node origin) {
 		if (origin == null) return null;
-		Node copy = new Node(origin.val, null);
+		List<Node> neighbors = new ArrayList<>(origin.neighbors.size());
+		Node copy = new Node(origin.val, neighbors);
 		return copy;
 	}
 	
+	
+	public static void main(String[] args) {
+		int[] height = {1,8,6,2,5,4,8,3,7};
+		
+		long t1 = System.nanoTime();
+		ContainerWithMostWater obj = 
+				new ContainerWithMostWater();
+		int maxValue = obj.maxArea2(height);
+		long t2 = System.nanoTime();
+		
+		System.out.println("output: "+maxValue);
+		System.out.println("Rumtime: "+(t2-t1)/1.0E6+" ms");
+	}
 }
