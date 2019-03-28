e8cea010729fd23fec45da7d4019ee4059fc4d4b
Apollo4634 <wenyuexin@163.com>
2019-03-29 01:01:10 +0800

    0329-0100 207 Course Schedule

---
+++ solution/depth_first_search/CourseSchedule.java
@@ -0,0 +1,19 @@
+package depth_first_search;
+
+/** 
+ * @author Apollo4634 
+ * @create 2019/03/29
+ * @problem 207
+ * @related Breadth-first Search
+ * @related Graph
+ * @related Topological Sort
+ * @see CourseSchedule_207
+ */
+
+public class CourseSchedule {
+
+	public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+		return true;
+    }
+}
