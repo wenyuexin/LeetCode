103d8a4f41876ecbdacc2e6113284dd82f901cb5
Apollo4634 <apollo4634@gmail.com>
2019-03-30 01:05:18 +0800

    0330-0104 207 Course Schedule - update

--- solution/depth_first_search/CourseSchedule.java
+++ solution/depth_first_search/CourseSchedule.java
@@ -1,9 +1,17 @@
 package depth_first_search;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import graph.Node;
+
 /** 
+ * 
+ * 
  * @author Apollo4634 
  * @create 2019/03/29
  * @problem 207
+ * @related Depth-first Search
  * @related Breadth-first Search
  * @related Graph
  * @related Topological Sort
@@ -12,8 +20,35 @@ package depth_first_search;
 
 public class CourseSchedule {
 
+	private Node[] nodeList;
+	private boolean[] marked;
+	
 	public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
+		nodeList = new Node[numCourses+1];
+		marked = new boolean[numCourses+1];
+		
+        Node root = new Node(0, new ArrayList<Node>(numCourses));
+		for (int i = 0; i < numCourses+1; i++) {
+			nodeList[i] = new Node(i, new LinkedList<Node>());
+			root.neighbors.add(nodeList[i]);
+		}
+		
+		for (int i = 0; i < numCourses; i++) {
+			for (int j = 0; j < prerequisites[i].length; j++) {
+				nodeList[i].neighbors.add(nodeList[prerequisites[i][j]]);
+			}
+		}
+		
+		visit(0);
+		
 		return true;
     }
+	
+	private void visit(int i) {
+		for (Node node : nodeList[i].neighbors) {
+			if (marked[i]) {
+				
+			}
+		}
+	}
 }
