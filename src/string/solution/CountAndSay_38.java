f29f71813f511be5c13d6f58e188a94b3ccc25da
Apollo4634 <apollo4634@gmail.com>
2019-06-29 16:28:15 +0800

    0629-1627 38 Count and Say - modify

--- src/string/solution/CountAndSay_38.java
+++ src/string/solution/CountAndSay_38.java
@@ -10,9 +10,40 @@ package string.solution;
 public class CountAndSay_38 {
     static class Solution {
         public String countAndSay(int n) {
-            String sequence = null;
+            StringBuilder sb = new StringBuilder(2*n);
+            sb.append("11");
 
-            return sequence;
+            for (int i = 1; i < n; i++) {
+                char ch = ' ';
+                int cnt = 1;
+                for (int j = 0; j < sb.length(); ) {
+                    if (sb.charAt(j) != ch) {
+                        String str = ""+cnt+sb.charAt(j);
+                        sb.replace(j-cnt+1, j+1, str);
+                        j = j - cnt + str.length();
+                        ch = sb.charAt(j);
+                        cnt = 1;
+                    } else {
+                        j += 1;
+                        cnt += 1;
+                    }
+                }
+                System.out.println(""+i+" "+sb);
+            }
+            return sb.toString();
         }
+    }
+
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Input:  "+num);
+
+        long t1 = System.nanoTime();
+        String sequence = new Solution().countAndSay(num);
+        long t2 = System.nanoTime();
+
+        System.out.println("Output: "+sequence);
+        System.out.println("Runtime: "+(t2-t1)/1.0E6+" ms");
     }
 }
