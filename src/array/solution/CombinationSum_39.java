ca0d5fa00c34339ba49e57e510189fb08f4548b9
Apollo4634 <apollo4634@gmail.com>
2019-07-04 21:25:00 +0800

    0704-2124 39 Combination Sum - modify

--- src/array/solution/CombinationSum_39.java
+++ src/array/solution/CombinationSum_39.java
@@ -1,6 +1,7 @@
 package array.solution;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -13,11 +14,38 @@ import java.util.List;
 
 public class CombinationSum_39 {
     static class Solution {
+        Integer[] set;
+        int idx;
+        int sum;
+        List<List<Integer>> combinations;
+
         public List<List<Integer>> combinationSum(int[] candidates, int target) {
-            List<List<Integer>> list = new ArrayList<>();
+            Arrays.sort(candidates);
+            int capacity = (int) (1.0 * target / candidates[0]) + 1;
+            set = new Integer[capacity];
+            sum = 0;
+            idx = 0;
+            combinations = new LinkedList<>();
+            for (int i: candidates) {
+                search(0, candidates, i, target);
+            }
+            return combinations;
+        }
 
-
-            return list;
+        private void search(int idx, int[] candidates, int from, int target) {
+            int s = sum + candidates[from];
+            if (s == target) {
+                set[idx] = candidates[from];
+                idx += 1;
+                combinations.add(Arrays.asList(set));
+            } else if (s < target) {
+                set[idx] = candidates[from];
+                idx += 1;
+                search(candidates, from, target);
+            } else {
+                from += 1;
+                search(candidates, from, target);
+            }
         }
     }
 }
