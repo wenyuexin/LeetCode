b52cea2b74c20c4f388c2d4f0de76d0a29d3bcff
Apollo4634 <apollo4634@gmail.com>
2019-07-05 00:29:53 +0800

    0705-0029 39 Combination Sum - modify

--- src/array/solution/CombinationSum_39.java
+++ src/array/solution/CombinationSum_39.java
@@ -14,42 +14,54 @@ import java.util.List;
 
 public class CombinationSum_39 {
     static class Solution {
+        int[] nums;
         Integer[] set;
-        int idx;
-        int sum;
+        //int lastIdx;
         List<List<Integer>> combinations;
 
         public List<List<Integer>> combinationSum(int[] candidates, int target) {
-            Arrays.sort(candidates);
+            nums = candidates;
+            Arrays.sort(nums);
             int capacity = (int) (1.0 * target / candidates[0]) + 1;
             set = new Integer[capacity];
-            sum = 0;
-            idx = 0;
             combinations = new LinkedList<>();
-            for (int i: candidates) {
-                search(0, candidates, i, target);
+            for (int i = 0; i < candidates.length; i++) {
+                search(0, i, target);
             }
             return combinations;
         }
 
-        private boolean search(int idx, int[] candidates, int from, int target) {
-            for (int i = from; i < candidates.length; i++) {
-                int s = sum + candidates[from];
-                if (s == target) {
-                    set[idx] = candidates[from];
-                    idx += 1;
-                    combinations.add(Arrays.asList(set));
-                } else if (s < target) {
-                    set[idx] = candidates[from];
-                    idx += 1;
-                    if (!search(idx+1, candidates, from, target)) {
-                        return false;
-                    }
+        private void search(int last, int from, int target) {
+            for (int i = from; i < nums.length; i++) {
+                if (nums[i] == target) {
+                    set[last] = nums[i];
+                    combinations.add(Arrays.asList(Arrays.copyOfRange(set, 0, last+1)));
+                    System.out.println("= "+ Arrays.toString(Arrays.copyOfRange(set, 0, last+1)) + " " + last);
+                } else if (nums[i] < target) {
+                    set[last] = nums[i];
+                    search(last+1, from, target-nums[i]);
+                    System.out.println("< "+ Arrays.toString(Arrays.copyOfRange(set, 0, last+1)) + " " + last);
                 } else {
-                    return false;
+                    set[last] = nums[i];
+                    System.out.println("> "+ Arrays.toString(Arrays.copyOfRange(set, 0, last+1)) + " " + last);
                 }
             }
-            return true;
         }
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[] { 2,3,6,7 };
+        int target = 7;
+
+        System.out.println("Input:  "+ Arrays.toString(nums));
+        System.out.println("Input:  "+target);
+
+        long t1 = System.nanoTime();
+        List<List<Integer>> list = new Solution().combinationSum(nums, target);
+        long t2 = System.nanoTime();
+
+        System.out.println("Output: "+ list.toString());
+        System.out.println("Runtime: "+(t2-t1)/1.0E6+" ms");
     }
 }
