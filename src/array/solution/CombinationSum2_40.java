9bdd34bc3c01a87a8793521bd6a64914d7a3dee6
Apollo4634 <apollo4634@gmail.com>
2019-07-05 16:26:52 +0800

    0705-1626 40 Combination Sum 2 - add

---
+++ src/array/solution/CombinationSum2_40.java
@@ -0,0 +1,24 @@
+package array.solution;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Apollo4634
+ * @create 2019/07/05
+ * @problem 40
+ * @tag Array
+ * @tag Backtracking
+ * @see array.solution.CombinationSum2_40
+ */
+
+public class CombinationSum2_40 {
+    static class Solution {
+        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+            List<List<Integer>> combination = new LinkedList<>();
+
+
+            return combination;
+        }
+    }
+}
