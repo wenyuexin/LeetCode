b468ecbb543413e131b5e48e3bbacd4c80a845aa
Apollo4634 <apollo4634@gmail.com>
2019-07-05 17:30:54 +0800

    0705-1730 40 Combination Sum 2 - modify

--- src/array/solution/CombinationSum2_40.java
+++ src/array/solution/CombinationSum2_40.java
@@ -1,5 +1,6 @@
 package array.solution;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,11 +15,52 @@ import java.util.List;
 
 public class CombinationSum2_40 {
     static class Solution {
+        List<List<Integer>> combinations;
+
         public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-            List<List<Integer>> combination = new LinkedList<>();
+            combinations = new LinkedList<>();
+            if (candidates == null || candidates.length == 0) return combinations;
 
-
-            return combination;
+            Arrays.sort(candidates);
+            List<Integer> set = new LinkedList<>();
+            search(set, candidates, 0, target);
+            return combinations;
         }
+
+        private void search(List<Integer> set, int[] nums, int from, int target) {
+            for (int i = from; i < nums.length; i++) {
+                if (nums[i] == target) {
+                    set.add(nums[i]);
+                    combinations.add(new LinkedList<>(set));
+                    //System.out.println("= "+set.toString());
+                    set.remove(set.size()-1);
+                    break;
+                } else if (nums[i] < target) {
+                    set.add(nums[i]);
+                    //System.out.println("< "+set.toString());
+                    search(set, nums, i+1, target-nums[i]);
+                    set.remove(set.size()-1);
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        //int[] nums = new int[] { 2,3,6,7 };
+        //int target = 7;
+
+        int[] nums = new int[] { 10,1,2,7,6,1,5 };
+        int target = 8;
+
+        System.out.println("Input:  "+ Arrays.toString(nums));
+        System.out.println("Input:  "+target);
+
+        long t1 = System.nanoTime();
+        List<List<Integer>> list = new Solution().combinationSum2(nums, target);
+        long t2 = System.nanoTime();
+
+        System.out.println("Output: "+ list.toString());
+        System.out.println("Runtime: "+(t2-t1)/1.0E6+" ms");
     }
 }
