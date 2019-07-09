6d36cdecab26ece443b2abc66d4d72f02fca95dc
Apollo4634 <apollo4634@gmail.com>
2019-07-09 21:58:17 +0800

    0709-2157 43 Multiply Strings - add

--- problem/math/0043_MultiplyStrings.md
+++ problem/math/0043_MultiplyStrings.md
@@ -2,8 +2,6 @@
 
 ```Medium, Math, String```
 
-1048480Share
-
 Given two non-negative integers `num1` and `num2` represented as strings, return the product of `num1` and `num2`, also represented as a string.
 
 **Example 1:**
--- src/math/solution/MultiplyStrings_43.java
+++ src/math/solution/MultiplyStrings_43.java
@@ -10,14 +10,56 @@ package math.solution;
 
 public class MultiplyStrings_43 {
     static class Solution {
-        public String multiply(String num1, String num2) {
+        StringBuilder sb;
+        char[] arr;
 
-            return "";
+        public String multiply(String num1, String num2) {
+            if ("0".equals(num1) || "0".equals(num2)) return "0";
+            if ("1".equals(num1)) return num2;
+            if ("1".equals(num2)) return num1;
+            sb = new StringBuilder();
+
+            if (num1.length() > num2.length()) {
+                String temp = num1;
+                num1 = num2;
+                num2 = temp;
+            }
+            arr = num1.toCharArray();
+
+            int c = 0;
+            for (int i = 0; i < num2.length(); i++) {
+                c = calc(i,num2.charAt(i) - '0', c);
+            }
+
+            if (c > 0) sb.append(c);
+            return sb.reverse().toString();
+        }
+
+        private int calc(int idx, int n2, int c) {
+            int sum = c;
+            for (int i = 0; i < arr.length; i++) {
+                int n1 = arr[i] - '0';
+                sum += (arr[i]*n2);
+            }
+            c = sum / 10;
+            sb.append((c > 0)? sum%10 : sum);
+            return c;
         }
     }
 
 
     public static void main(String[] args) {
+        String num1 = "45";
+        String num2 = "123";
 
+        System.out.println("Input:  "+num1);
+        System.out.println("Input:  "+num2);
+
+        long t1 = System.nanoTime();
+        String product = new Solution().multiply(num1, num2);
+        long t2 = System.nanoTime();
+
+        System.out.println("Output: "+product);
+        System.out.println("Runtime: "+(t2-t1)/1.0E6+" ms");
     }
 }
