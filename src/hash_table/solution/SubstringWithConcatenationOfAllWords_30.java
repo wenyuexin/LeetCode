b7402f544d67f06376c66bc52b05e5a146e8409f
Apollo4634 <apollo4634@gmail.com>
2019-07-29 23:40:21 +0800

    0729-2337 30 Substring with Concatenation of All Words - solved

--- src/hash_table/solution/SubstringWithConcatenationOfAllWords_30.java
+++ src/hash_table/solution/SubstringWithConcatenationOfAllWords_30.java
@@ -22,65 +22,55 @@ import java.util.Map;
 
 public class SubstringWithConcatenationOfAllWords_30 {
 
-	//Solution 1
-	public List<Integer> findSubstring(String s, String[] words) {
-		List<Integer> list = new LinkedList<Integer>();
-		if (s==null || words==null || words.length==0) return list;
-		int wordLen = words[0].length();
-		int totalLen = words.length * wordLen;
-		if (s.length() < totalLen) return list;
+	static class Solution {
+		public List<Integer> findSubstring(String s, String[] words) {
+			List<Integer> list = new LinkedList<>();
+			if (s==null || words==null || words.length==0) return list;
+			int wordLen = words[0].length();
+			int totalLen = words.length * wordLen;
+			if (s.length() < totalLen) return list;
 
-		Map<String, Integer> map = new HashMap<String,Integer>();
-		for (int i = 0; i < words.length; i++) {
-			if (!map.keySet().contains(words[i])) map.put(words[i], 1);
-			else map.put(words[i], map.get(words[i])+1);
+			Map<String, Integer> map = new HashMap<>();
+			for (String value : words) {
+				if (!map.containsKey(value)) map.put(value, 1);
+				else map.put(value, map.get(value) + 1);
+			}
+
+			int j;
+			int max = s.length() - totalLen;
+			Map<String, Integer> tempMap = new HashMap<>();
+			for (int left = 0; left <= max; left++) {
+				for (j = 0; j < words.length; j++) {
+					int idx = left + j*wordLen;
+					String word = s.substring(idx, idx + wordLen);
+					if (!map.containsKey(word)) { tempMap.clear(); break; }
+					int cnt = tempMap.getOrDefault(word, 0);
+					if (cnt + 1 > map.get(word)) { tempMap.clear(); break; }
+					tempMap.put(word, cnt+1);
+				}
+				if (j == words.length) list.add(left);
+				tempMap.clear();
+			}
+			return list;
 		}
-		
-		int j = 0;
-		int max = s.length() - totalLen;
-		Map<String, Integer> tempMap = new HashMap<>();
-		for (int left = 0; left <= max; left++) {
-			for (j = 0; j < words.length; j++) {
-				int idx = left + j*wordLen;
-				String word = s.substring(idx, idx + wordLen);
-				if (!map.keySet().contains(word)) { tempMap.clear(); break; }
-				int cnt = tempMap.getOrDefault(word, 0);
-				if (cnt + 1 > map.get(word)) { tempMap.clear(); break; }
-				tempMap.put(word, cnt+1);
-			}	
-			if (j == words.length) list.add(left);
-			tempMap.clear();
-		}
-		return list;
-	}
-
-
-	//Solution 2
-	public List<Integer> findSubstring2(String s, String[] words) {
-		List<Integer> list = new LinkedList<Integer>();
-		
-		
-		return list;
 	}
 	
 	
 	public static void main(String[] args) {		
-		//String s = "barfoothefoobarman";
-		//String[] words = new String[] { "foo","bar" }; //0,9
+		String s = "barfoothefoobarman";
+		String[] words = new String[] { "foo","bar" }; //0,9
 
 		//String s = "wordgoodgoodgoodbestword";
 		//String[] words = new String[] { "word","good","best","good" };//8
 
-		String s = "wordgoodgoodgoodbestword";
-		String[] words = new String[] { "word","good","best","word" };//[]
+		//String s = "wordgoodgoodgoodbestword";
+		//String[] words = new String[] { "word","good","best","word" };//[]
 				
 		System.out.println("Input:  "+s);
 		System.out.println("Input:  "+Arrays.toString(words));
 
 		long t1 = System.nanoTime();
-		SubstringWithConcatenationOfAllWords_30 obj =
-				new SubstringWithConcatenationOfAllWords_30();
-		List<Integer> list = obj.findSubstring(s, words);
+		List<Integer> list = new Solution().findSubstring(s, words);
 		long t2 = System.nanoTime();
 
 		System.out.println("Output: "+list);
