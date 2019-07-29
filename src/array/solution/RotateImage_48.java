ad53170f1e3f51c126887914c399e2457e603bdb
Apollo4634 <apollo4634@gmail.com>
2019-07-29 17:16:50 +0800

    0729-1716 48 Rotate Image - solved

--- src/array/solution/RotateImage_48.java
+++ src/array/solution/RotateImage_48.java
@@ -3,9 +3,8 @@ package array.solution;
 import java.util.Arrays;
 
 /**
- * 将矩阵顺时针选择90度
- *
- * 思路：每次选定4个数进行旋转变换
+ * 要求：将矩阵顺时针选择90度
+ * 思路：逐层旋转，每次选定4个数进行变换
  *
  * @author Apollo4634
  * @create 2019/07/28
@@ -14,6 +13,7 @@ import java.util.Arrays;
  */
 
 public class RotateImage_48 {
+    //Recommend
     static class Solution {
         public void rotate(int[][] matrix) {
 
@@ -21,23 +21,36 @@ public class RotateImage_48 {
             for (int i = 0; i < layerNum; i++) {
                 int rotateTimes = matrix.length - 2*i - 1;
                 for (int j = 0; j < rotateTimes; j++) {
-                    //todo swap
+                    swap(matrix, i, matrix.length-1-i, j);
                 }
             }
         }
 
-        void swap(int[][] matrix, int r, int c) {
-
+        void swap(int[][] matrix, int b1, int b2, int offset) {
+            int tmp = matrix[b1][b1+offset];//top-left
+            matrix[b1][b1+offset] = matrix[b2-offset][b1];//bottom-left -> top-left
+            matrix[b2-offset][b1] = matrix[b2][b2-offset];//bottom-right -> bottom-left
+            matrix[b2][b2-offset] = matrix[b1+offset][b2];//top-right -> bottom-left
+            matrix[b1+offset][b2] = tmp;//top-left -> top-right
+            //System.out.println(Arrays.deepToString(matrix));
         }
     }
 
 
     public static void main(String[] args) {
+//        int[][] matrix = new int[][] {
+//                { 1,2,3 },
+//                { 4,5,6 },
+//                { 7,8,9 }
+//        };
+
         int[][] matrix = new int[][] {
-                { 1,2,3 },
-                { 4,5,6 },
-                { 7,8,9 }
+                { 0,1,2,3 },
+                { 10,11,12,13 },
+                { 20,21,22,23 },
+                { 30,31,32,33 }
         };
+
         System.out.println("Input:  "+ Arrays.deepToString(matrix));
 
         long start = System.nanoTime();
