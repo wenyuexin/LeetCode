831a7e5ca4f3ff70013ed517bd286c82069a5903
Apollo4634 <wenyuexin@163.com>
2019-08-01 11:27:44 +0800

    0801-1127 50 Pow(x,n)

--- src/math/solution/PowXN_50.java
+++ src/math/solution/PowXN_50.java
@@ -1,5 +1,8 @@
 package math.solution;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Apollo4634
  * @create 2019/07/31
@@ -13,17 +16,61 @@ package math.solution;
 public class PowXN_50 {
     static class Solution {
         public double myPow(double x, int n) {
-            if (x == 0 && n <= 0) return 0; //无意义
+            return Math.pow(x, n);
+        }
+    }
+
+
+    static class Solution2 {
+        public double myPow(double x, int n) {
             if (n == 0) return 1;
+            if (n == 1) return x;
+            if (Double.isNaN(x)) return Double.NaN;
+            if (x == Double.POSITIVE_INFINITY && n > 0) return Double.POSITIVE_INFINITY;
+            if (x == Double.POSITIVE_INFINITY && n < 0) return 0;
+            if (x == Double.NEGATIVE_INFINITY && n > 0) return Double.NEGATIVE_INFINITY;
+            if (x == Double.NEGATIVE_INFINITY && n < 0) return 0;
+            if (x == 0 && n < 0) return Double.NaN;
+            if (x == 0 && n > 0) return 1.0;
+            return calcPow(x, n);
+        }
 
-            double ans = 0;
-            //Math.pow();
+        private double calcPow(double x, int n) {
+            boolean xIsPositive = true;
+            if (n % 2 == 1 && (Math.abs(x) / x < 0)) xIsPositive = false;
+            x = Math.abs(x);
+            boolean nIsPositive = (Math.abs(n) / n > 0);
+            n = Math.abs(n); //有问题
 
+            double ans = calc(x, n);
+            if (!xIsPositive) ans = -ans;
+            if (!nIsPositive) ans = 1/ans;
             return ans;
         }
 
-        private void calc() {
-
+        private double calc(double x, int n) {
+            if (n < 3) {
+                if (n == 0) return 1;
+                if (n == 1) return x;
+                return x * x;
+            }
+            double ans = calc(x, n/2);
+            return (n%2==1) ? x*ans*ans : ans*ans;
         }
+    }
+
+
+    public static void main(String[] args) {
+        double x = 2.; // -100.0 < x < 100.0
+        int n = Integer.MIN_VALUE;
+        System.out.println("Input:  " + "x = " + x);
+        System.out.println("Input:  " + "n = " + n);
+
+        long t1 = System.nanoTime();
+        double ans = new Solution2().myPow(x, n);
+        long t2 = System.nanoTime();
+
+        System.out.println("Output: " + ans);
+        System.out.println("Runtime: " + (t2 - t1) / 1.0E6 + " ms");
     }
 }
