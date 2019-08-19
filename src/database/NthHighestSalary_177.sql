0dc6d21822f670cd4fe9a8b46a3f0db4b0440269
Apollo4634 <apollo4634@gmail.com>
2019-08-19 17:04:12 +0800

    0819-1704 177 Nth Highest Salary - solved

--- src/database/NthHighestSalary_177.sql
+++ src/database/NthHighestSalary_177.sql
@@ -7,6 +7,7 @@
 -- END
 
 
+-- Solution 1
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
     set N = N - 1;
@@ -21,4 +22,36 @@ BEGIN
              limit 1 offset N), null
         ) as nthHighestSalary
     );
+END
+
+
+-- Solution 2
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  SET N = N - 1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT DISTINCT Salary FROM Employee
+      ORDER BY Salary DESC LIMIT 1 OFFSET N
+  );
+END
+
+
+-- Solution 3
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    IF (
+        SELECT COUNT(DISTINCT Salary)
+        FROM Employee
+        ) < N
+    THEN RETURN NULL;
+    ELSE
+        SET N = N-1;
+        RETURN (
+            SELECT DISTINCT Salary
+            FROM Employee
+            ORDER BY Salary DESC
+            LIMIT N,1
+        );
+    END IF;
 END
