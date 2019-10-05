59a63e2088dd4f928c2cee32b58088019abd7ed8
Apollo4634 <apollo4634@gmail.com>
2019-10-05 22:42:46 +0800

    1005-2242 91 Decode Ways - solved

--- src/string/solution/DecodeWays_91.java
+++ src/string/solution/DecodeWays_91.java
@@ -1,9 +1,8 @@
 package string.solution;
 
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author Apollo4634
@@ -11,6 +10,8 @@ import java.util.Map;
  */
 
 public class DecodeWays_91 {
+
+    //unrecommended
     static class Solution {
         private int count;
         private int maxIdx;
@@ -39,13 +40,62 @@ public class DecodeWays_91 {
     }
 
 
+    //recommended
+    static class Solution2 {
+        private int count;
+
+        public int numDecodings(String s) {
+            if (s == null || s.length() == 0) return 0;
+            char[] chars = s.toCharArray();
+
+            List<Integer> list = new LinkedList<>() {{ add(0); }};
+            for (int i = 0; i < chars.length-1; i++) {
+                if (chars[i] != '1' && chars[i] != '2') list.add(i+1);
+            }
+            list.add(chars.length);
+
+            Iterator<Integer> iter = list.iterator();
+            int from = iter.next();
+            int totalCount = 1;
+            while (iter.hasNext()) {
+                if (chars[from] == '0') {
+                    totalCount = 0; break;
+                }
+                int to = iter.next();
+                if (from+1 == to) continue;
+                count = 0;
+                decode(chars, from, to);
+                totalCount *= count;
+                if (totalCount == 0) break;
+                from = to;
+            }
+            return totalCount;
+        }
+
+        private void decode(char[] chars,  int idx, int to) {
+            if (idx >= to) { count += 1; return; }
+            if (chars[idx] == '0') return;
+            if (idx < to-1) {
+                if (chars[idx] == '1' || (chars[idx] == '2' && chars[idx+1] < '7')) {
+                    decode(chars, idx+2, to);
+                }
+            }
+            decode(chars, idx+1, to);
+        }
+    }
+
+
     public static void main(String[] args) {
+        //String s = "0";
+        //String s = "1";
         //String s = "123";
-        String s = "93715976311287769483871971322671886773499467423442178461584251978418763151253";
+        String s = "12318";
+        //String s = "93715976311287";
+        //String s = "9371597631128776948387197132267188";
         System.out.println("Input:  "+s);
 
         long t1 = System.nanoTime();
-        int ret = new Solution().numDecodings(s);
+        int ret = new Solution2().numDecodings(s);
         long t2 = System.nanoTime();
 
         System.out.println("Output: "+ret);
