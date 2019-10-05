1510eec4e3338409d7fd03aedfaf02bc998ac2e8
Apollo4634 <apollo4634@gmail.com>
2019-10-05 11:42:11 +0800

    1005-1141 91 Decode Ways - solved

--- src/string/solution/DecodeWays_91.java
+++ src/string/solution/DecodeWays_91.java
@@ -12,50 +12,36 @@ import java.util.Map;
 
 public class DecodeWays_91 {
     static class Solution {
-        private static Map<String, Character> map = new HashMap<>() {{
-            map.put("10", 'J'); map.put("11", 'K'); map.put("12", 'L');
-            map.put("13", 'M'); map.put("14", 'N'); map.put("15", 'O');
-            map.put("16", 'P'); map.put("17", 'Q'); map.put("18", 'R'); map.put("19", 'S');
-            map.put("20", 'T'); map.put("21", 'U'); map.put("22", 'V');
-            map.put("23", 'W'); map.put("24", 'X'); map.put("25", 'Y'); map.put("26", 'Z');
-        }};
-
-        private List<String> list;
+        private int count;
+        private int maxIdx;
 
         public int numDecodings(String s) {
             if (s == null || s.length() == 0) return 0;
-            list = new LinkedList<>();
-            StringBuilder sb = new StringBuilder();
-            decode(s, 0, sb);
-            return list.size();
+            count = 0;
+            maxIdx = s.length() - 1;
+            decode(s, 0);
+            return count;
         }
 
-        private void decode(String s, int idx, StringBuilder sb) {
-            int maxIdx = s.length() - 1;
-            char ch = s.charAt(idx);
-            if (idx > maxIdx || ch == '0') return;
-            if (idx == maxIdx) {
-                list.add(sb.toString());
+        private void decode(String s, int idx) {
+            if (idx > maxIdx) {
+                count += 1; return;
             }
-
+            char ch = s.charAt(idx);
+            if (ch == '0') return;
             if (idx < maxIdx) {
                 if (ch == '1' || (ch == '2' && s.charAt(idx+1) < '7')) {
-                    String sub = s.substring(idx, idx+2);
-                    sb.append(map.get(sub));
-                    decode(s, idx+2, sb);
-                    sb.deleteCharAt(sb.length()-1);
+                    decode(s, idx+2);
                 }
             }
-
-            sb.append((char) (s.charAt(idx) - '1' + 'A'));
-            decode(s, idx+1, sb);
-            sb.deleteCharAt(sb.length()-1);
+            decode(s, idx+1);
         }
     }
 
 
     public static void main(String[] args) {
-        String s = "123";
+        //String s = "123";
+        String s = "93715976311287769483871971322671886773499467423442178461584251978418763151253";
         System.out.println("Input:  "+s);
 
         long t1 = System.nanoTime();
