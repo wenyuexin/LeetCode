a63d22ce18e50a42b7ccb1c67ac35d63d08521a1
Apollo4634 <apollo4634@gmail.com>
2019-10-15 23:28:57 +0800

    1015-2328 198 House Robber

--- src/dynamic_programming/solution/HouseRobber_198.java
+++ src/dynamic_programming/solution/HouseRobber_198.java
@@ -28,26 +28,41 @@ public class HouseRobber_198 {
         }
     }
 
-    //unrecommended
+    //unrecommended,
     static class Solution2 {
         public int rob(int[] nums) {
             if (nums == null || nums.length == 0) return 0;
             if (nums.length == 1) return nums[0];
+            if (nums.length == 2) return Math.max(nums[0], nums[1]);
+            if (nums.length == 3) return Math.max(nums[0]+nums[2], nums[1]);
 
-            return 0;
+            int prev= nums[0] + nums[2];
+            int cur = -1;
+            int max = Integer.MIN_VALUE;
+            int idx = -2;
+            for (int i = 3; i < nums.length; i++) {
+                cur = nums[i] + Math.max(nums[i-3], nums[i-2]);
+                if (cur > prev) {
+                    idx = i;
+                } else {
+                    idx = i - 1;
+                }
+            }
+
+            return idx < nums.length-1 ? prev : cur;
         }
     }
 
 
     public static void main(String[] args) {
-        //int[] nums = new int[] { 2,7,9,3,1 };
-        //int[] nums = new int[] { 1,2,3,1 };
-        int[] nums = new int[] {
-                226,174,214,16,218,48,153,131,128,17,157,142,88,43,37,157,43,221,191,
-                68,206,23,225,82,54,118,111,46,80,49,245,63,25,194,72,80,143,55,209,
-                18,55,122,65,66,177,101,63,201,172,130,103,225,142,46,86,185,62,138,
-                212,192,125,77,223,188,99,228,90,25,193,211,84,239,119,234,85,83,123,
-                120,131,203,219,10,82,35,120,180,249,106,37,169,225,54,103,55,166,124 };
+        int[] nums = new int[] { 2,7,9,3,1,4,5 };
+//        int[] nums = new int[] { 1,2,3,1 };
+//        int[] nums = new int[] {
+//                226,174,214,16,218,48,153,131,128,17,157,142,88,43,37,157,43,221,191,
+//                68,206,23,225,82,54,118,111,46,80,49,245,63,25,194,72,80,143,55,209,
+//                18,55,122,65,66,177,101,63,201,172,130,103,225,142,46,86,185,62,138,
+//                212,192,125,77,223,188,99,228,90,25,193,211,84,239,119,234,85,83,123,
+//                120,131,203,219,10,82,35,120,180,249,106,37,169,225,54,103,55,166,124 };
         System.out.println("Output: "+ Arrays.toString(nums));
 
         long start = System.nanoTime();
