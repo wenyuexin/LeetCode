10647553ccb2d033acafd0f3e90a89e4df565ac1
Apollo4634 <apollo4634@gmail.com>
2020-01-20 20:34:00 +0800

    0120-2032 101 Symmetric Tree - solved

--- src/tree/solution/SymmetricTree_101.java
+++ src/tree/solution/SymmetricTree_101.java
@@ -2,16 +2,35 @@ package tree.solution;
 
 import tree.utils.TreeNode;
 
+import java.util.LinkedList;
+
 public class SymmetricTree_101 {
     static class Solution {
         public boolean isSymmetric(TreeNode root) {
-
-            return true;
+            if (root == null) return true;
+            LinkedList<TreeNode> nodes = new LinkedList<>();
+            nodes.add(root.left);
+            nodes.add(root.right);
+            return bfs(nodes);
         }
 
-        private boolean bfs(TreeNode root) {
-
-
+        private boolean bfs(LinkedList<TreeNode> currentLayer) {
+            LinkedList<TreeNode> nextLayer;
+            while (!currentLayer.isEmpty()) {
+                nextLayer = new LinkedList<>();
+                while (!currentLayer.isEmpty()) {
+                    TreeNode firstNode = currentLayer.pollFirst();
+                    TreeNode lastNode = currentLayer.pollLast();
+                    if (firstNode == null && lastNode == null) continue;
+                    if (firstNode == null ^ lastNode == null) return false;
+                    if (firstNode.val != lastNode.val) return false;
+                    nextLayer.addFirst(firstNode.left);
+                    nextLayer.addFirst(firstNode.right);
+                    nextLayer.addLast(lastNode.right);
+                    nextLayer.addLast(lastNode.left);
+                }
+                currentLayer = nextLayer;
+            }
             return true;
         }
     }
